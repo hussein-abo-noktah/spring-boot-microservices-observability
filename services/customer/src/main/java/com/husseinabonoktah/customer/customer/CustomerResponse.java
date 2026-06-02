e19030974ca0853b817533c1166fa64ebc2f50af
hussein-abo-noktah <89044400+hussein-abo-noktah@users.noreply.github.com>
2026-06-03 03:42:01 +0400

    customer management crud

---
+++ services/customer/src/main/java/com/husseinabonoktah/customer/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.husseinabonoktah.customer.customer;
+
+public record CustomerResponse(
+    String id,
+    String firstname,
+    String lastname,
+    String email,
+    Address address
+) {
+
+}
