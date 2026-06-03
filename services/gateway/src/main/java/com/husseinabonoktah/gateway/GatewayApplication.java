4410b7bac4a2fffbdd721c2532b11b4f9cce69a9
hussein-abo-noktah <89044400+hussein-abo-noktah@users.noreply.github.com>
2026-06-04 01:09:48 +0400

    config gateway service

---
+++ services/gateway/src/main/java/com/husseinabonoktah/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.husseinabonoktah.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
